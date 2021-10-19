afc2e2c89ae740bf9635b6de22a98729e989541a
kaikarh <54577978+kaikarh@users.noreply.github.com>
2021-10-19 17:27:26 +0800

    Update index.tsx
    import Head component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
